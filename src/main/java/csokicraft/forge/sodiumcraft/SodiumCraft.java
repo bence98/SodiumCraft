ef57383166c9f5a53f7b851555e27c0090828bac
bence98 <bence.csokas@gmail.com>
2017-12-26 12:24:38 +0100

    Fixed the Mekanism version number in the dependency declaration

--- src/main/java/csokicraft/forge/sodiumcraft/SodiumCraft.java
+++ src/main/java/csokicraft/forge/sodiumcraft/SodiumCraft.java
@@ -32,12 +32,12 @@ import net.minecraftforge.fml.common.event.*;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import mekanism.api.gas.*;
 
-@Mod(modid = SodiumCraft.MODID, version = SodiumCraft.VERSION, dependencies="required-after:mekanism@[9.4,);required-after:thermalexpansion@[5.3,)")
+@Mod(modid = SodiumCraft.MODID, version = SodiumCraft.VERSION, dependencies="required-after:mekanism@[1.12.2-9.4,);required-after:thermalexpansion@[5.3,)")
 @EventBusSubscriber
 public class SodiumCraft
 {
 	public static final String MODID = "sodiumcraft";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 
 	@SidedProxy(serverSide="csokicraft.forge.sodiumcraft.CommonProxy", clientSide="csokicraft.forge.sodiumcraft.ClientProxy")
 	public static CommonProxy proxy;
